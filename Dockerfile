b1da7acde43c190a0d20bae1a269b1a8c0d89fd0
cesarmarroquin <cesarm@pigeon.ly>
2019-01-28 23:49:44 -0800

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -7,42 +7,12 @@ ENV PYTHONUNBUFFERED 1
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV DJANGO_ENV dev
 
-#RUN apk add --no-cache postgresql-client
-
 RUN apt-get update \
         && apt-get install -y openssh-client \
         && pip install pipenv
-
-
-#RUN useradd wagtail
-#RUN chown -R wagtail ..
-#USER wagtail
-#
-### Copy the current directory contents into the container at /code/
-#RUN mkdir /code
-#RUN chown -R wagtail /code
-#COPY .. /code/
-## Set the working directory to /code/
-#WORKDIR /code/
 
 RUN mkdir /code/
 WORKDIR /code/
 ADD . /code/
 
-RUN pwd
-RUN ls
-#RUN docker ps -a
-
-#ADD requirements.txt /code/
 RUN pip install -r requirements.txt
-
-#COPY ../requirements.txt /code/requirements.txt
-#RUN pip install --upgrade pip
-# Install any needed packages specified in requirements.txt
-#RUN pip install -r /code/requirements.txt
-#RUN pip install gunicorn
-
-#RUN python manage.py migrate
-#
-#EXPOSE 8000
-#CMD exec gunicorn base.wsgi:application --bind 0.0.0.0:8000 --workers 3
--- home/tests.py
+++ home/tests.py
@@ -1,53 +1,9 @@
-def test_example_test():
-    assert 1 + 1 == 2
-
-
-
-
- # class BaseExampleEndpointTests(APITestCase):
-#     def setUp(self):
-#         # User fixtures
-#         self.email = 'homersimpson@gmail.com'
-#         self.first_name = 'homer'
-#         self.last_name = 'simpson'
-#         self.password = 'Test123q!'
-#         self.user = User.objects.create_user(username=self.email, email=self.email, password=self.password,
-#                                              first_name=self.first_name, last_name=self.last_name)
-#         self.user_token = Token.objects.filter(user=self.author).first()
-#
-#         self.example1 = Example.objects.create()
-#
-#         self.example1_serializer_instance = ExampleListRetrieveSerializer(instance=self.example1)
-#
-#
-# class ExampleListCreateTests(BaseExampleEndpointTests):
-#     def setUp(self):
-#         super().setUp()
-#         self.url = reverse('example_list')
-#
-#     def test_example_list(self):
-#         examples = Example.objects.all()
-#         count = examples.count()
-#         self.assertEqual(count, 1)
-
-
-
-
-
-
 from django.test import TestCase
-# from myapp.models import Animal
+
 
 class AnimalTestCase(TestCase):
     def setUp(self):
         pass
-        # Animal.objects.create(name="lion", sound="roar")
-        # Animal.objects.create(name="cat", sound="meow")
 
     def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        # lion = Animal.objects.get(name="lion")
-        # cat = Animal.objects.get(name="cat")
-        # self.assertEqual(lion.speak(), 'The lion says "roar"')
-        # self.assertEqual(cat.speak(), 'The cat says "meow"')
         assert 1 + 1 == 2

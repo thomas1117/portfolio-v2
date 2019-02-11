923af2e23ae49498a7135cca27104de1c454ff49
Thomas Shannon <thomas@develoship.com>
2019-02-11 00:43:51 +0000

    Revert "Merge branch '11-layout' into 'master'"
    This reverts merge request !1

--- base/static/src/js/main.js
+++ base/static/src/js/main.js
@@ -1,9 +1,9 @@
 import '../sass/main.scss';
 
-import '@/utils/scroll';
-
 import Test from '@/vue/components/Test';
 import Vue from 'vue';
+
+console.log('yolo');
 
 const app = new Vue({
     el: '#app',
